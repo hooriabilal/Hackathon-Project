18a232f057c767c9e4534f1555181dc09514a7ae
Hooria Bilal <167758500+hooriabilal@users.noreply.github.com>
2024-11-07 23:28:17 +0500

    Add files via upload

---
+++ Hackathon/Milestone 1-2/script.js
@@ -0,0 +1,4 @@
+// Simple TypeScript function to display an alert when the resume loads
+window.onload = function () {
+    console.log("Resume loaded successfully!");
+};
---
+++ Hackathon/Milestone 1-2/script.ts
@@ -0,0 +1,4 @@
+// Simple TypeScript function to display an alert when the resume loads
+window.onload = () => {
+    console.log("Resume loaded successfully!");
+};
